d796382affd7a194735a47a3135c9ef857b665a7
Ivan <example@gmail.com>
2023-09-25 17:03:28 +0300

    add h3

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ console.log(--decr);
 
 console.log(5%2);
 
-console.log(2*4 === 8);
+console.log(2+2 * 2 !== 8);
 const isChecked =  false,
 	  isClose = false;
 console.log(isChecked || !isClose);
