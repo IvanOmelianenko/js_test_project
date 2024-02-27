3c0e05fe3368495e5fd373956df34f987d2bc16a
Ivan <vanyokredray@gmail.com>
2024-02-27 17:52:32 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,47 +1,121 @@
 "use strict";
-/* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number);
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+// const obj = {
+//   a: 50,
+// };
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+// obj.a = 10;
 
-Проверить, чтобы все работало без ошибок в консоли */
+// console.log(obj);
+// var name = "Ivan";
+// console.log(name);
 
-const numberOfFilms = +prompt("How many films are you watched?", "");
+// {
+//   var result = 50;
+// }
+// console.log(result);
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
+// alert(5);
+// [].push("a");
+// const carBodyWidth = 5000, vehicleBodyLength = 4000;
+// console.log("Width of car: " + vehicleBodyWidth + ", length: " + vehicleBodyLength);
+// ['fsfdsf', 'dfdsf', 'fdfdfdfd'].map(a => )
+// Request
+// data
+// response
+//snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+// const COLOR_RED = '#F00';
+// let number = 4.6;
+// console.log(-4 / 0);
+// console.log("string" * 9);
+// const person = "5";
+// const bool = false;
+// console.log(something);
+// let und;
+// console.log(und);
 
-};
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+// // console.log(obj.name);
+// console.log(obj["name"]);
 
-const lastWatched = prompt("What film your watch last?", ""),
-	score = prompt("What is your score?", ""),
-	lastWatched1 = prompt("What film your watch last?", ""),
-	score1 = prompt("What is your score?", "");
+// let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(arr[1]);
+// const arr = ["a", "b", "c"];
 
-personalMovieDB.movies[lastWatched] = score;
-personalMovieDB.movies[lastWatched1]= score1;
-console.log(personalMovieDB);
+// arr[10] = "3456";
+// console.log(arr);
 
+// const arrObj = {
+//   a: "a",
+//   1: "b",
+//   2: "c",
+//   abc: {
+//     df: [{}, {}],
+//     def: {},
+//   },
+// };
 
+// const b = "b";
 
+// // arrObj.b = "123";
+// arrObj["b"] = [];
 
+// console.log(arrObj["b"]);
+// console.log(arrObj.b);
+// // const obj = {a: 1, b: 2};
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// };
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "18");
+// console.log(answer + 510);
+
+// const answers = [];
+// (answers[0] = prompt("What is your name", "")),
+//   (answers[1] = prompt("What is your surname", "")),
+//   (answers[2] = prompt("What is your age", ""));
+// console.log(typeof answers);
+// console.log(typeof null);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Hello, ${user}`);
+
+console.log("arr" + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+++incr;
+--decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== "6");
+
+const isChecked = false,
+  isClose = false;
+console.log(isChecked || !isClose);
---
+++ js/script1.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// const hamburger= 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+// 	console.log("I'm done");
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+
+// console.log(1 && 5);
+
+// console.log(null && 5);
+
+// console.log(0 && "dsdsdsds");
+
+// if (hamburger === 3 && cola ===1 && fries) {
+// 	console.log("All done");
+// } else {
+// 	console.log("Go to another cafe");
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log("Allright");
+// } else {
+// 	console.log("Go to another cafe");
+// }
+
+console.log( NaN && 2 && undefined );
+
+
+let johnReport, alexReport, sampReport, mariaReport = "done";
+
+
